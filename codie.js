d610a612ded936249bad0072cb7b805a4b8c605e
Zoltan Bettenbuk <zoltan.bettenbuk@gmail.com>
2016-11-16 20:59:38 +0100

    Add queries and menus

--- codie.js
+++ codie.js
@@ -15,8 +15,8 @@
       method: "GET",
       dataType: "jsonp",
       data: request,
-      success: function(data) {
-        cb(data);
+      success: function(response) {
+        cb(response.data);
       }
     });
   }
@@ -47,10 +47,10 @@
     }, cb);
   };
 
-  ext.turn = function(angle, speed, cb) {
+  ext.turn = function(direction, angle, speed, cb) {
     sendCodieRequest("/commands/turn", {
       angle: angle,
-      speed: speed
+      speed: direction === "right" ? speed * -1 : speed
     }, cb);
   };
 
@@ -72,7 +72,7 @@
 
   ext.setAnimation = function(animation, hue, saturation, repeat, speed, cb) {
     sendCodieRequest("/commands/setAnimation", {
-      animation: animation,
+      animation: animation === "round" ? 0 : 1,
       hue: hue,
       saturation: saturation,
       repeat: repeat,
@@ -80,18 +80,50 @@
     }, cb);
   };
 
+  ext.getDistance = function(cb) {
+    sendCodieRequest("/queries/distance", cb);
+  };
+
+  ext.getBatery = function(cb) {
+    sendCodieRequest("/queries/battery", cb);
+  };
+
+  ext.getSound = function(cb) {
+    sendCodieRequest("/queries/sound", cb);
+  };
+
+  ext.getLight = function(cb) {
+    sendCodieRequest("/queries/light", cb);
+  };
+
+  ext.getLine = function(cb) {
+    sendCodieRequest("/queries/line", cb);
+  };
+
   // Block and block menu descriptions
   var descriptor = {
     blocks: [
-      ["w", "Move %n mm with %n speed", "move"],
-      ["w", "Enable motors. Left %n Right %n", "enableMotorBoth"],
-      ["w", "Set left motor speed to %n", "enableMotorLeft"],
-      ["w", "Set right motor speed to %n", "enableMotorRight"],
-      ["w", "Turn %n degrees with %n speed", "turn"],
-      ["w", "Beep on %n volume, %n frequency and %n duration", "beep"],
-      ["w", "Set color to H %n S %n V %n", "setColor"],
-      ["w", "Start animation %n with H %n S%, repeat %n times and % speed", "setAnimation"]
-    ]
+      // commands
+      ["w", "Move %n mm with %n speed", "move", 100, 50],
+      ["w", "Enable both motors. Left %n Right %n", "enableMotorBoth", 50, 50],
+      ["w", "Set left motor speed to %n", "enableMotorLeft", 50],
+      ["w", "Set right motor speed to %n", "enableMotorRight", 50],
+      ["w", "Turn %m.turnDirection %n degrees with %n speed", "turn", "left", 90, 50],
+      ["w", "Beep on %n volume, %n frequency and %n duration", "beep", 50, 440, 100],
+      ["w", "Set color to H %n S %n V %n", "setColor", 50, 100, 100],
+      ["w", "Start animation %m.animation with H %n S n%, repeat %n times and % speed", "setAnimation", "round", 50, 50, 5, 50],
+
+      // queries
+      ["R", "Distance", "getDistance"],
+      ["R", "Battery", "getBattery"],
+      ["R", "Sound", "getSound"],
+      ["R", "Light", "getLight"],
+      ["R", "Line", "getLine"],
+    ],
+    menus: {
+      turnDirection: ["left", "right"],
+      animation: ["round", "flash"]
+    }
   };
 
   // Register the extension
