e935f2914c8948578a1b7b0e645eed9674798b5e
Zoltan Bettenbuk <zoltan.bettenbuk@gmail.com>
2016-11-16 20:10:01 +0100

    Move command

--- codie.js
+++ codie.js
@@ -1,21 +1,37 @@
 (function(ext) {
-    ext._shutdown = function() {};
+  var baseUrl = "http://localhost:8080";
+  ext._shutdown = function() {};
 
-    ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
+  ext._getStatus = function() {
+    return {
+      status: 2,
+      msg: "Ready"
     };
+  };
 
-    ext.move = function (distance, speed, cb) {
-      console.log("Move", distance, "mm with", speed, "speed");
-    };
+  ext.move = function(distance, speed, cb) {
+    console.log("Move", distance, "mm with", speed, "speed");
+    $.ajax({
+      url: baseUrl + "/commands/move",
+      method: "POST",
+      dataType: "jsonp",
+      data: {
+        distance: distance,
+        speed: speed
+      },
+      success: function(finished) {
+        callback();
+      }
+    });
+  };
 
-    // Block and block menu descriptions
-    var descriptor = {
-        blocks: [
-          ["w", "move %n mm with %n speed", "move"]
-        ]
-    };
+  // Block and block menu descriptions
+  var descriptor = {
+    blocks: [
+      ["w", "move %n mm with %n speed", "move"]
+    ]
+  };
 
-    // Register the extension
-    ScratchExtensions.register('Codie extension', descriptor, ext);
+  // Register the extension
+  ScratchExtensions.register("Codie extension", descriptor, ext);
 })({});
